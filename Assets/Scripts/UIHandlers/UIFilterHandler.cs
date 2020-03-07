810c4ccf3e4a6666220a6078a498fa70689199cc
PiePeter527 <piepeter527@hotmail.com>
2020-03-07 15:42:10 -0400

    Updated filter amount text, and added search filtering functionality

--- Assets/Scripts/UIHandlers/UIFilterHandler.cs
+++ Assets/Scripts/UIHandlers/UIFilterHandler.cs
@@ -5,16 +5,42 @@ using UnityEngine.UI;
 
 public class UIFilterHandler : MonoBehaviour
 {
-    [SerializeField] Text _pokemonFilterAmountStatus;
+    [SerializeField] Text _filterAmountStatus;
+
+    [SerializeField] Button _searchClearButton;
+    [SerializeField] InputField _searchField;
 
     //TODO: Continue here with Filtering
 
     [SerializeField] DataHandler _dataHandler;
     [SerializeField] FilterHandler _filterHandler;
 
+    void Start()
+    {
+        UpdateFilterAmountStatus();
+    }
+
+    public void SearchClearClicked()
+    {
+        _searchClearButton.gameObject.SetActive(false);
+        _searchField.text = "";
+        _filterHandler.SetNameFilter("");
+        UpdateFilterAmountStatus();
+    }
+
+    public void SearchInputChanged()
+    {
+        string input = _searchField.text;
+
+        _searchClearButton.gameObject.SetActive(input != "");
+        _filterHandler.SetNameFilter(input);
+        UpdateFilterAmountStatus();
+    }
+
+    //To get called after filter changes
     void UpdateFilterAmountStatus()
     {
-        _pokemonFilterAmountStatus.text = "Displaying "
+        _filterAmountStatus.text = "Displaying "
             + _filterHandler.NumberPokemonFilterMatched 
             + "/"
             + _dataHandler.NumberOfPokemon
