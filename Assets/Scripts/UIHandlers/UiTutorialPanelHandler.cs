959010efa4ff05b0a039e4a2daf9ce68f004f102
PiePeter527 <piepeter527@hotmail.com>
2020-04-03 22:07:20 -0300

    Added a tutorial panel at start

---
+++ Assets/Scripts/UIHandlers/UiTutorialPanelHandler.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiTutorialPanelHandler : MonoBehaviour
+{
+    [SerializeField] GameObject _overlay;
+
+    public void OnReadyClicked()
+    {
+        _overlay.SetActive(false);
+        gameObject.SetActive(false);
+    }
+}
